e9bfcab9264948bc36e015ccf630ca8efb0467e2
Ofek Shochat <o@shoch.at>
2021-01-09 22:25:12 +0200

    client

--- pythonScripts/testing/client.py
+++ pythonScripts/testing/client.py
@@ -19,113 +19,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import socket
 import threading
+import socketserver
+from glob import glob
+import os
+import json
 import requests
-import hashlib 
 
-key = input("api_key: ")
-result = hashlib.sha256(key.encode()) 
-print(result.hexdigest())
-apikeys = requests.get("https://raw.githubusercontent.com/OfekShochat/0xA2_chessengine/master/testing/passwords.dat", allow_redirects=True).content.decode("utf-8").split("\n")
-if not result.hexdigest() in apikeys:
-    exit(0)
+VERSION = 0.1
 
-r = requests.get("https://raw.githubusercontent.com/OfekShochat/0xA2_chessengine/master/testing/book.txt", allow_redirects=True)
-open('book.txt', 'wb').write(r.content)
-
-"""
-def client(ip, port, message):
+def client(ip, port, mtype):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
     try:
+        if mtype == "register":
+            message = b"""{"type":"register"}"""
+
         sock.sendall(message)
-        response = sock.recv(1024)
+        response = sock.recv(4096)
     finally:
         sock.close()
-"""
-import chess
-import chess.engine as ce
-import random
-from time import time
-import chess.pgn
-from discord_webhook import DiscordWebhook
-from glob import glob
-import sys
-import os 
+    return response
 
-class engine:
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
-        self.rating = 0 
-        self.games = 0
-        self.en = ce.SimpleEngine.popen_uci(path)
+if __name__=="__main__":
+    try:
+        if not open("login.txt").read() == "":
+            apikey = json.loads(open("login.txt"))["key"]
+        raise FileNotFoundError
+    except FileNotFoundError:
+        print("no login information. getting api key")
+        open("login.txt", "w+").write(client("127.0.0.1", 41378, "register").decode("utf-8"))
 
-engines = []
-for i in sys.argv[1:]:
-    engines.append(engine(i.split("\\")[-1], i))
-
-book = open("book.txt", "r").readlines()
-st = time()
-for i in range(10000):
-    moves = []
-    engines_sort = sorted(engines, key=lambda x: x.games + random.random()*100)
-    engine1 = engines_sort[0]
-    engine2 = engines_sort[1]
-    wc = 3 * 60
-    bc = 3 * 60
-    dd = False
-    d = False
-    print("\n\nstarting a match (gameno {}): {} {} vs. {} {}".format(i, str(engine1.name), engine1.rating, str(engine2.name), engine2.rating))
-    book_choice = random.choice(book)
-    board = chess.Board(book_choice)
-
-    while not board.is_game_over():
-        e1s = time()
-        m = engine1.en.play(board, ce.Limit(white_clock=wc, black_clock=bc))
-        board.push(m.move)
-        moves.append(str(m.move))
-        if m.draw_offered:
-            if d:
-                dd = True
-                break
-            d = True
-        else:
-            d = False
-            
-        wc -= (time() - e1s)
-        #wc += 1 
-        if board.is_game_over(): break
-        e2s = time()
-        m = engine1.en.play(board, ce.Limit(white_clock=wc, black_clock=bc))
-        if m.draw_offered:
-            if d:
-                dd = True
-                break
-            d = True
-        else:
-            d = False
-        board.push(m.move)
-        moves.append(str(m.move))
-        bc -= (time() - e2s)
-        #bc += 1
-        ##print(board)
-        print("{} {} | {} {}                              ".format(engine1.name, wc, engine2.name, bc), end="\r")
-        if wc < 0 or bc < 0:
-            break
-    r = board.result()
+    if not ".\\cutechess" in glob(".\\**"):
+        import zipfile
+        with zipfile.ZipFile(".\\cutechess.zip", 'r') as zip_ref:
+            os.mkdir(".\\cutechess")
+            zip_ref.extractall(".\\cutechess")
     
-    if wc < 0:
-        open("game.pgn", "w+").write("[Result \"0-1\"]\n")
-    elif bc < 0:
-        open("game.pgn", "w+").write("[Result \"1-0\"]\n")
-    else:
-        open("game.pgn", "w+").write("[Result \"{}\"]\n".format(r))   
-    open("game.pgn", "a+").write("[FEN \"{}\"]\n".format(book_choice[:book_choice.find("\n")]))
-    open("game.pgn", "a+").write("[White \"{}\"]\n".format(engine1.name))
-    open("game.pgn", "a+").write("[Black \"{}\"]\n".format(engine2.name))
-    b = chess.Board(book_choice)
-    bb = b.variation_san([chess.Move.from_uci(m) for m in moves]) 
-    open("game.pgn", "a+").write(str(bb) + "\n\n")
-    #client("127.0.0.1", 41378, open("game.pgn", "rb").read())
-    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/794818234238763040/ZrlRJx-tEC3hF8Z4pLURa_xqOoyqSR51DNjTvWVdGjtKKDGLVdUl74S83IyE8QJB9vYh', content=open("game.pgn").read())
-    response = webhook.execute()
+    r = requests.get("https://raw.githubusercontent.com/OfekShochat/0xA2_chessengine/master/testing/book.txt", allow_redirects=True)
+    open('book.txt', 'wb').write(r.content)
+
+    os.system(r""".\cutechess\cutechess-cli.exe -engine name=lc0 cmd=D:\lc0-v0.26.1-windows-gpu-nvidia-cuda\lc0.exe"""
+    + r""" -engine name=stockfish cmd=D:\stockfish_12_win_x64_bmi2\stockfish_20090216_x64_bmi2.exe -each proto=uci tc=100/90+1 """
+    + r"""-rounds 1 -pgnout file.pgn min fi -bookmode disk -openings file=book_3moves_cp25-49_13580pos.pgn format=pgn""")
--- pythonScripts/testing/server.py
+++ pythonScripts/testing/server.py
@@ -16,26 +16,54 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 import socket
 import threading
 import socketserver
 from time import sleep
+from time import time
+import json 
+import hashlib
+from terminaltables import AsciiTable
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-
     def handle(self):
         data = self.request.recv(1024)
-        for line in data.decode('utf-8').split('\n'):
-            open("games.pgn", "a+").write(line)
-        open("games.pgn", "a+").write("\n\n")
+        d = json.loads(data.decode('utf-8'))
+    
+        if d["type"] == "register":
+            key = hashlib.md5(bytes(str(time()), "utf-8")).hexdigest()
+            self.request.sendall(b"""{"type":"registerResponse", "key":%s}""" % bytes(key, "utf-8"))
+
+        if d["type"] == "jobGet":
+            self.request.sendall(b"""{"type:"GetResponse", "job":%s}""" % jobs[0])
+            inprogress.append({"job":job[0], "time":time()})
+            jobs.pop(0)
+            
+        if d["type"] == "finishJob":
+            self.request.sendall(b"""{"type":"finishResponse"}""")
+            for line in d["game"].split('\n'):
+                open("games5.pgn", "a+").write(line)
+            open("games5.pgn", "a+").write("\n\n")
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
+def checker():
+    while True:
+        for i in inprogress:
+            if time() - i["time"] > 180*2+20:
+                jobs.append(i["job"])
+                inprogress.pop(inprogress.index(i))
+
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "127.0.0.1", 41378
+
+    jobs = []
+    inprogress = []
+    r = open("jobs.txt", "r").readlines()
+    for i in r:
+        jobs.append(json.loads(i))
 
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     ip, port = server.server_address
@@ -46,7 +74,19 @@ if __name__ == "__main__":
     # Exit the server thread when the main thread terminates
     server_thread.daemon = True
     server_thread.start()
+
+    checker_Thread = threading.Thread(target=checker)
+    checker_Thread.deamon = True
+    checker_Thread.start()
+    
     while True:
-        sleep(3)
+        i = input("% ")
+        if i.lower() == "addjob":
+            jobs.append(json.loads(input("addJob: ")))
+        if i.lower() == "peek":
+            t = [["job", "status"]]
+            print(t)
+            table = AsciiTable(t)
+            print(table.table)
     server.shutdown()
     server.server_close()
